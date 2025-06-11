cafd7343175eeb69c68f75ba281e8960378099af
Akansha mishra <131146760+mishra-akansha@users.noreply.github.com>
2025-06-11 16:29:35 +0530

    fourth

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main {
   public static void main(String[] args) {
     while (true) {
-      System.out.println("Running infinitely...v2");
+      System.out.println("Running infinitely...v3");
       
       // Optional: Add a small delay to avoid overwhelming the console 
       try {
