e3a03987c2ef4b672c0a226054d54a9ffae77344
Akansha mishra <131146760+mishra-akansha@users.noreply.github.com>
2025-06-11 16:27:38 +0530

    third commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+// Write your Java code here
+public class Main {
+  public static void main(String[] args) {
+    while (true) {
+      System.out.println("Running infinitely...v2");
+      
+      // Optional: Add a small delay to avoid overwhelming the console 
+      try {
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
